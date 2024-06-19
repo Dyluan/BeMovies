a7770e720b9741858cc131e8604a2685bdaa15ff
Henry <ayala.hd98@gmail.com>
2024-06-19 14:11:23 +0200

    HTML/CSS-Henry-Done

---
+++ bemovies/app.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        clickable: true,
+    },
+});
+
